a2a39b600aa71f4194e950aab82c7e337f3cd057
Jeremy Alcala <jeremyalcalaxcz@hotmail.com>
2023-12-22 17:24:50 -0500

    second version

--- js/script.js
+++ js/script.js
@@ -83,7 +83,7 @@ function changeBackground() {
 
 
 // Ocultar el popup y establecer el fondo inicial al cargar la página
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const popup = document.getElementById('popup');
     popup.style.display = 'none';
 
@@ -131,3 +131,150 @@ function typeWriter(text) {
 
     type();
 }
+
+
+
+
+const puzzleContainer = document.getElementById('puzzle');
+const initialPuzzleState = [
+    ['img/1.jpg', 'img/2.jpg', 'img/3.jpg'],
+    ['img/4.jpg', 'img/5.jpg', 'img/6.jpg'],
+    ['img/7.jpg', 'img/8.jpg', 'img/empty.jpg']
+];
+
+let currentPuzzleState = [...initialPuzzleState];
+let isPuzzleSolved = false;
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function initializePuzzle() {
+    puzzleContainer.innerHTML = '';
+
+    currentPuzzleState = [...initialPuzzleState];
+
+    if (!isPuzzleSolved) {
+        const flattenedPuzzle = currentPuzzleState.flat();
+        shuffleArray(flattenedPuzzle);
+
+        for (let i = 0; i < currentPuzzleState.length; i++) {
+            for (let j = 0; j < currentPuzzleState[i].length; j++) {
+                currentPuzzleState[i][j] = flattenedPuzzle[i * currentPuzzleState.length + j];
+            }
+        }
+    }
+
+    currentPuzzleState.forEach((row, rowIndex) => {
+        row.forEach((imageName, colIndex) => {
+            const cell = document.createElement('div');
+            cell.className = 'puzzle-cell';
+
+            if (imageName !== 'img/empty.jpg') {
+                const img = document.createElement('img');
+                img.src = imageName;
+                img.className = 'puzzle-img'; // Agrega una clase para el estilo de la imagen
+                img.style.width = '100%'; // Establece el ancho de la imagen
+                img.style.height = '100%'; // Establece la altura de la imagen
+                cell.appendChild(img);
+            }
+
+            cell.addEventListener('click', () => moveTile(rowIndex, colIndex));
+            puzzleContainer.appendChild(cell);
+        });
+    });
+
+    isPuzzleSolved = false;
+
+    // Deshabilita el botón al inicio
+    document.getElementById('siguienteBtn').disabled = true;
+}
+
+function moveTile(row, col) {
+    if (!isPuzzleSolved) {
+        // Busca la posición del espacio vacío ('img/empty.jpg')
+        const emptyRow = currentPuzzleState.findIndex(row => row.includes('img/empty.jpg'));
+        const emptyCol = currentPuzzleState[emptyRow].indexOf('img/empty.jpg');
+
+        if (isValidMove(row, col, emptyRow, emptyCol)) {
+            swapTiles(row, col, emptyRow, emptyCol);
+            updatePuzzleView();
+            checkPuzzleSolved();
+        }
+    }
+}
+
+function isValidMove(row, col, emptyRow, emptyCol) {
+    return (
+        (Math.abs(row - emptyRow) === 1 && col === emptyCol) ||
+        (Math.abs(col - emptyCol) === 1 && row === emptyRow)
+    );
+}
+
+function swapTiles(row1, col1, row2, col2) {
+    const temp = currentPuzzleState[row1][col1];
+    currentPuzzleState[row1][col1] = currentPuzzleState[row2][col2];
+    currentPuzzleState[row2][col2] = temp;
+}
+
+function updatePuzzleView() {
+    puzzleContainer.innerHTML = '';
+
+    currentPuzzleState.forEach((row, rowIndex) => {
+        row.forEach((imageName, colIndex) => {
+            const cell = document.createElement('div');
+            cell.className = 'puzzle-cell';
+
+            if (imageName !== 'img/empty.jpg') {
+                const img = document.createElement('img');
+                img.src = imageName;
+                img.className = 'puzzle-img'; // Agrega una clase para el estilo de la imagen
+                img.style.width = '100%'; // Establece el ancho de la imagen
+                img.style.height = '100%'; // Establece la altura de la imagen
+                cell.appendChild(img);
+            }
+
+            cell.addEventListener('click', () => moveTile(rowIndex, colIndex));
+            puzzleContainer.appendChild(cell);
+        });
+    });
+}
+
+
+function checkPuzzleSolved() {
+    const flattenedState = currentPuzzleState.flat();
+    const emptyIndex = flattenedState.indexOf('img/empty.jpg');
+
+    // Verifica si todos los elementos están en la posición correcta en relación con 'img/empty.jpg'
+    const isSolved = flattenedState.every((imageName, index) => {
+        if (imageName === 'img/empty.jpg') {
+            return index === flattenedState.length - 1; // 'img/empty.jpg' debe estar en la última posición
+        } else {
+            const expectedImage = `img/${index + 1}.jpg`;
+            return imageName === expectedImage;
+        }
+    });
+    const filtered = flattenedState.filter((_, index) => index !== emptyIndex);
+    console.log(filtered);
+    const siguienteBtn = document.getElementById('siguienteBtn');
+    console.log(flattenedState);
+    console.log(emptyIndex);
+    console.log(isSolved);
+    if (isSolved) {
+        isPuzzleSolved = true;
+        puzzleContainer.classList.add('puzzle-solved');
+        siguienteBtn.style.display = 'block';
+        siguienteBtn.disabled = false;
+        console.log("resuelto");
+    } else {
+        isPuzzleSolved = false;
+        puzzleContainer.classList.remove('puzzle-solved');
+        siguienteBtn.disabled = true;
+        console.log("falta");
+    }
+}
+
+initializePuzzle();
